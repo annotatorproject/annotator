98207d7922b3a7d57df8d9fdac798b2ecfbe308f
Christian <chriamue@gmail.com>
2017-01-19 21:48:58 +0100

    added annotatorlib_features.h for cmake version compatibility

---
+++ codegeneration/annotatorplugin_features.h
@@ -0,0 +1,475 @@
+
+// This is a generated file. Do not edit!
+
+#ifndef ANNOTATORPLUGIN_COMPILER_DETECTION_H
+#define ANNOTATORPLUGIN_COMPILER_DETECTION_H
+
+#ifdef __cplusplus
+#define ANNOTATORPLUGIN_COMPILER_IS_Comeau 0
+#define ANNOTATORPLUGIN_COMPILER_IS_Intel 0
+#define ANNOTATORPLUGIN_COMPILER_IS_PathScale 0
+#define ANNOTATORPLUGIN_COMPILER_IS_Embarcadero 0
+#define ANNOTATORPLUGIN_COMPILER_IS_Borland 0
+#define ANNOTATORPLUGIN_COMPILER_IS_Watcom 0
+#define ANNOTATORPLUGIN_COMPILER_IS_OpenWatcom 0
+#define ANNOTATORPLUGIN_COMPILER_IS_SunPro 0
+#define ANNOTATORPLUGIN_COMPILER_IS_HP 0
+#define ANNOTATORPLUGIN_COMPILER_IS_Compaq 0
+#define ANNOTATORPLUGIN_COMPILER_IS_zOS 0
+#define ANNOTATORPLUGIN_COMPILER_IS_XL 0
+#define ANNOTATORPLUGIN_COMPILER_IS_VisualAge 0
+#define ANNOTATORPLUGIN_COMPILER_IS_PGI 0
+#define ANNOTATORPLUGIN_COMPILER_IS_Cray 0
+#define ANNOTATORPLUGIN_COMPILER_IS_TI 0
+#define ANNOTATORPLUGIN_COMPILER_IS_Fujitsu 0
+#define ANNOTATORPLUGIN_COMPILER_IS_SCO 0
+#define ANNOTATORPLUGIN_COMPILER_IS_AppleClang 0
+#define ANNOTATORPLUGIN_COMPILER_IS_Clang 0
+#define ANNOTATORPLUGIN_COMPILER_IS_GNU 0
+#define ANNOTATORPLUGIN_COMPILER_IS_MSVC 0
+#define ANNOTATORPLUGIN_COMPILER_IS_ADSP 0
+#define ANNOTATORPLUGIN_COMPILER_IS_IAR 0
+#define ANNOTATORPLUGIN_COMPILER_IS_MIPSpro 0
+
+#if defined(__COMO__)
+#undef ANNOTATORPLUGIN_COMPILER_IS_Comeau
+#define ANNOTATORPLUGIN_COMPILER_IS_Comeau 1
+
+#elif defined(__INTEL_COMPILER) || defined(__ICC)
+#undef ANNOTATORPLUGIN_COMPILER_IS_Intel
+#define ANNOTATORPLUGIN_COMPILER_IS_Intel 1
+
+#elif defined(__PATHCC__)
+#undef ANNOTATORPLUGIN_COMPILER_IS_PathScale
+#define ANNOTATORPLUGIN_COMPILER_IS_PathScale 1
+
+#elif defined(__BORLANDC__) && defined(__CODEGEARC_VERSION__)
+#undef ANNOTATORPLUGIN_COMPILER_IS_Embarcadero
+#define ANNOTATORPLUGIN_COMPILER_IS_Embarcadero 1
+
+#elif defined(__BORLANDC__)
+#undef ANNOTATORPLUGIN_COMPILER_IS_Borland
+#define ANNOTATORPLUGIN_COMPILER_IS_Borland 1
+
+#elif defined(__WATCOMC__) && __WATCOMC__ < 1200
+#undef ANNOTATORPLUGIN_COMPILER_IS_Watcom
+#define ANNOTATORPLUGIN_COMPILER_IS_Watcom 1
+
+#elif defined(__WATCOMC__)
+#undef ANNOTATORPLUGIN_COMPILER_IS_OpenWatcom
+#define ANNOTATORPLUGIN_COMPILER_IS_OpenWatcom 1
+
+#elif defined(__SUNPRO_CC)
+#undef ANNOTATORPLUGIN_COMPILER_IS_SunPro
+#define ANNOTATORPLUGIN_COMPILER_IS_SunPro 1
+
+#elif defined(__HP_aCC)
+#undef ANNOTATORPLUGIN_COMPILER_IS_HP
+#define ANNOTATORPLUGIN_COMPILER_IS_HP 1
+
+#elif defined(__DECCXX)
+#undef ANNOTATORPLUGIN_COMPILER_IS_Compaq
+#define ANNOTATORPLUGIN_COMPILER_IS_Compaq 1
+
+#elif defined(__IBMCPP__) && defined(__COMPILER_VER__)
+#undef ANNOTATORPLUGIN_COMPILER_IS_zOS
+#define ANNOTATORPLUGIN_COMPILER_IS_zOS 1
+
+#elif defined(__IBMCPP__) && !defined(__COMPILER_VER__) && __IBMCPP__ >= 800
+#undef ANNOTATORPLUGIN_COMPILER_IS_XL
+#define ANNOTATORPLUGIN_COMPILER_IS_XL 1
+
+#elif defined(__IBMCPP__) && !defined(__COMPILER_VER__) && __IBMCPP__ < 800
+#undef ANNOTATORPLUGIN_COMPILER_IS_VisualAge
+#define ANNOTATORPLUGIN_COMPILER_IS_VisualAge 1
+
+#elif defined(__PGI)
+#undef ANNOTATORPLUGIN_COMPILER_IS_PGI
+#define ANNOTATORPLUGIN_COMPILER_IS_PGI 1
+
+#elif defined(_CRAYC)
+#undef ANNOTATORPLUGIN_COMPILER_IS_Cray
+#define ANNOTATORPLUGIN_COMPILER_IS_Cray 1
+
+#elif defined(__TI_COMPILER_VERSION__)
+#undef ANNOTATORPLUGIN_COMPILER_IS_TI
+#define ANNOTATORPLUGIN_COMPILER_IS_TI 1
+
+#elif defined(__FUJITSU) || defined(__FCC_VERSION) || defined(__fcc_version)
+#undef ANNOTATORPLUGIN_COMPILER_IS_Fujitsu
+#define ANNOTATORPLUGIN_COMPILER_IS_Fujitsu 1
+
+#elif defined(__SCO_VERSION__)
+#undef ANNOTATORPLUGIN_COMPILER_IS_SCO
+#define ANNOTATORPLUGIN_COMPILER_IS_SCO 1
+
+#elif defined(__clang__) && defined(__apple_build_version__)
+#undef ANNOTATORPLUGIN_COMPILER_IS_AppleClang
+#define ANNOTATORPLUGIN_COMPILER_IS_AppleClang 1
+
+#elif defined(__clang__)
+#undef ANNOTATORPLUGIN_COMPILER_IS_Clang
+#define ANNOTATORPLUGIN_COMPILER_IS_Clang 1
+
+#elif defined(__GNUC__)
+#undef ANNOTATORPLUGIN_COMPILER_IS_GNU
+#define ANNOTATORPLUGIN_COMPILER_IS_GNU 1
+
+#elif defined(_MSC_VER)
+#undef ANNOTATORPLUGIN_COMPILER_IS_MSVC
+#define ANNOTATORPLUGIN_COMPILER_IS_MSVC 1
+
+#elif defined(__VISUALDSPVERSION__) || defined(__ADSPBLACKFIN__) || \
+    defined(__ADSPTS__) || defined(__ADSP21000__)
+#undef ANNOTATORPLUGIN_COMPILER_IS_ADSP
+#define ANNOTATORPLUGIN_COMPILER_IS_ADSP 1
+
+#elif defined(__IAR_SYSTEMS_ICC__) || defined(__IAR_SYSTEMS_ICC)
+#undef ANNOTATORPLUGIN_COMPILER_IS_IAR
+#define ANNOTATORPLUGIN_COMPILER_IS_IAR 1
+
+#elif defined(_SGI_COMPILER_VERSION) || defined(_COMPILER_VERSION)
+#undef ANNOTATORPLUGIN_COMPILER_IS_MIPSpro
+#define ANNOTATORPLUGIN_COMPILER_IS_MIPSpro 1
+
+#endif
+
+#if ANNOTATORPLUGIN_COMPILER_IS_AppleClang
+
+#if !(((__clang_major__ * 100) + __clang_minor__) >= 400)
+#error Unsupported compiler version
+#endif
+
+#define ANNOTATORPLUGIN_COMPILER_VERSION_MAJOR (__clang_major__)
+#define ANNOTATORPLUGIN_COMPILER_VERSION_MINOR (__clang_minor__)
+#define ANNOTATORPLUGIN_COMPILER_VERSION_PATCH (__clang_patchlevel__)
+#if defined(_MSC_VER)
+/* _MSC_VER = VVRR */
+#define ANNOTATORPLUGIN_SIMULATE_VERSION_MAJOR (_MSC_VER / 100)
+#define ANNOTATORPLUGIN_SIMULATE_VERSION_MINOR (_MSC_VER % 100)
+#endif
+#define ANNOTATORPLUGIN_COMPILER_VERSION_TWEAK (__apple_build_version__)
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 400 && \
+    __has_feature(cxx_alignas)
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNAS 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNAS 0
+#endif
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 400 && \
+    __has_feature(cxx_alignas)
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNOF 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNOF 0
+#endif
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 400 && \
+    __has_feature(cxx_constexpr)
+#define ANNOTATORPLUGIN_COMPILER_CXX_CONSTEXPR 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_CONSTEXPR 0
+#endif
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 400 && \
+    __has_feature(cxx_override_control)
+#define ANNOTATORPLUGIN_COMPILER_CXX_FINAL 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_FINAL 0
+#endif
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 400 && \
+    __has_feature(cxx_noexcept)
+#define ANNOTATORPLUGIN_COMPILER_CXX_NOEXCEPT 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_NOEXCEPT 0
+#endif
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 400 && \
+    __has_feature(cxx_nullptr)
+#define ANNOTATORPLUGIN_COMPILER_CXX_NULLPTR 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_NULLPTR 0
+#endif
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 400 && __cplusplus >= 201103L
+#define ANNOTATORPLUGIN_COMPILER_CXX_SIZEOF_MEMBER 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_SIZEOF_MEMBER 0
+#endif
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 400 && \
+    __has_feature(cxx_thread_local)
+#define ANNOTATORPLUGIN_COMPILER_CXX_THREAD_LOCAL 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_THREAD_LOCAL 0
+#endif
+
+#elif ANNOTATORPLUGIN_COMPILER_IS_Clang
+
+#if !(((__clang_major__ * 100) + __clang_minor__) >= 304)
+#error Unsupported compiler version
+#endif
+
+#define ANNOTATORPLUGIN_COMPILER_VERSION_MAJOR (__clang_major__)
+#define ANNOTATORPLUGIN_COMPILER_VERSION_MINOR (__clang_minor__)
+#define ANNOTATORPLUGIN_COMPILER_VERSION_PATCH (__clang_patchlevel__)
+#if defined(_MSC_VER)
+/* _MSC_VER = VVRR */
+#define ANNOTATORPLUGIN_SIMULATE_VERSION_MAJOR (_MSC_VER / 100)
+#define ANNOTATORPLUGIN_SIMULATE_VERSION_MINOR (_MSC_VER % 100)
+#endif
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 304 && \
+    __has_feature(cxx_alignas)
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNAS 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNAS 0
+#endif
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 304 && \
+    __has_feature(cxx_alignas)
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNOF 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNOF 0
+#endif
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 304 && \
+    __has_feature(cxx_constexpr)
+#define ANNOTATORPLUGIN_COMPILER_CXX_CONSTEXPR 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_CONSTEXPR 0
+#endif
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 304 && \
+    __has_feature(cxx_override_control)
+#define ANNOTATORPLUGIN_COMPILER_CXX_FINAL 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_FINAL 0
+#endif
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 304 && \
+    __has_feature(cxx_noexcept)
+#define ANNOTATORPLUGIN_COMPILER_CXX_NOEXCEPT 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_NOEXCEPT 0
+#endif
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 304 && \
+    __has_feature(cxx_nullptr)
+#define ANNOTATORPLUGIN_COMPILER_CXX_NULLPTR 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_NULLPTR 0
+#endif
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 304 && __cplusplus >= 201103L
+#define ANNOTATORPLUGIN_COMPILER_CXX_SIZEOF_MEMBER 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_SIZEOF_MEMBER 0
+#endif
+
+#if ((__clang_major__ * 100) + __clang_minor__) >= 304 && \
+    __has_feature(cxx_thread_local)
+#define ANNOTATORPLUGIN_COMPILER_CXX_THREAD_LOCAL 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_THREAD_LOCAL 0
+#endif
+
+#elif ANNOTATORPLUGIN_COMPILER_IS_GNU
+
+#if !((__GNUC__ * 100 + __GNUC_MINOR__) >= 404)
+#error Unsupported compiler version
+#endif
+
+#define ANNOTATORPLUGIN_COMPILER_VERSION_MAJOR (__GNUC__)
+#define ANNOTATORPLUGIN_COMPILER_VERSION_MINOR (__GNUC_MINOR__)
+#if defined(__GNUC_PATCHLEVEL__)
+#define ANNOTATORPLUGIN_COMPILER_VERSION_PATCH (__GNUC_PATCHLEVEL__)
+#endif
+
+#if (__GNUC__ * 100 + __GNUC_MINOR__) >= 408 && __cplusplus >= 201103L
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNAS 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNAS 0
+#endif
+
+#if (__GNUC__ * 100 + __GNUC_MINOR__) >= 408 && __cplusplus >= 201103L
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNOF 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNOF 0
+#endif
+
+#if (__GNUC__ * 100 + __GNUC_MINOR__) >= 406 && \
+    (__cplusplus >= 201103L ||                  \
+     (defined(__GXX_EXPERIMENTAL_CXX0X__) && __GXX_EXPERIMENTAL_CXX0X__))
+#define ANNOTATORPLUGIN_COMPILER_CXX_CONSTEXPR 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_CONSTEXPR 0
+#endif
+
+#if (__GNUC__ * 100 + __GNUC_MINOR__) >= 407 && __cplusplus >= 201103L
+#define ANNOTATORPLUGIN_COMPILER_CXX_FINAL 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_FINAL 0
+#endif
+
+#if (__GNUC__ * 100 + __GNUC_MINOR__) >= 406 && \
+    (__cplusplus >= 201103L ||                  \
+     (defined(__GXX_EXPERIMENTAL_CXX0X__) && __GXX_EXPERIMENTAL_CXX0X__))
+#define ANNOTATORPLUGIN_COMPILER_CXX_NOEXCEPT 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_NOEXCEPT 0
+#endif
+
+#if (__GNUC__ * 100 + __GNUC_MINOR__) >= 406 && \
+    (__cplusplus >= 201103L ||                  \
+     (defined(__GXX_EXPERIMENTAL_CXX0X__) && __GXX_EXPERIMENTAL_CXX0X__))
+#define ANNOTATORPLUGIN_COMPILER_CXX_NULLPTR 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_NULLPTR 0
+#endif
+
+#if (__GNUC__ * 100 + __GNUC_MINOR__) >= 404 && \
+    (__cplusplus >= 201103L ||                  \
+     (defined(__GXX_EXPERIMENTAL_CXX0X__) && __GXX_EXPERIMENTAL_CXX0X__))
+#define ANNOTATORPLUGIN_COMPILER_CXX_SIZEOF_MEMBER 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_SIZEOF_MEMBER 0
+#endif
+
+#if (__GNUC__ * 100 + __GNUC_MINOR__) >= 408 && __cplusplus >= 201103L
+#define ANNOTATORPLUGIN_COMPILER_CXX_THREAD_LOCAL 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_THREAD_LOCAL 0
+#endif
+
+#elif ANNOTATORPLUGIN_COMPILER_IS_MSVC
+
+#if !(_MSC_VER >= 1600)
+#error Unsupported compiler version
+#endif
+
+/* _MSC_VER = VVRR */
+#define ANNOTATORPLUGIN_COMPILER_VERSION_MAJOR (_MSC_VER / 100)
+#define ANNOTATORPLUGIN_COMPILER_VERSION_MINOR (_MSC_VER % 100)
+#if defined(_MSC_FULL_VER)
+#if _MSC_VER >= 1400
+/* _MSC_FULL_VER = VVRRPPPPP */
+#define ANNOTATORPLUGIN_COMPILER_VERSION_PATCH (_MSC_FULL_VER % 100000)
+#else
+/* _MSC_FULL_VER = VVRRPPPP */
+#define ANNOTATORPLUGIN_COMPILER_VERSION_PATCH (_MSC_FULL_VER % 10000)
+#endif
+#endif
+#if defined(_MSC_BUILD)
+#define ANNOTATORPLUGIN_COMPILER_VERSION_TWEAK (_MSC_BUILD)
+#endif
+
+#if _MSC_VER >= 1900
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNAS 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNAS 0
+#endif
+
+#if _MSC_VER >= 1900
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNOF 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_ALIGNOF 0
+#endif
+
+#define ANNOTATORPLUGIN_COMPILER_CXX_CONSTEXPR 0
+
+#if _MSC_VER >= 1700
+#define ANNOTATORPLUGIN_COMPILER_CXX_FINAL 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_FINAL 0
+#endif
+
+#if _MSC_VER >= 1900
+#define ANNOTATORPLUGIN_COMPILER_CXX_NOEXCEPT 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_NOEXCEPT 0
+#endif
+
+#if _MSC_VER >= 1600
+#define ANNOTATORPLUGIN_COMPILER_CXX_NULLPTR 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_NULLPTR 0
+#endif
+
+#if _MSC_VER >= 1900
+#define ANNOTATORPLUGIN_COMPILER_CXX_SIZEOF_MEMBER 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_SIZEOF_MEMBER 0
+#endif
+
+#if _MSC_VER >= 1900
+#define ANNOTATORPLUGIN_COMPILER_CXX_THREAD_LOCAL 1
+#else
+#define ANNOTATORPLUGIN_COMPILER_CXX_THREAD_LOCAL 0
+#endif
+
+#else
+#error Unsupported compiler
+#endif
+
+#if ANNOTATORPLUGIN_COMPILER_CXX_ALIGNAS
+#define ANNOTATORPLUGIN_ALIGNAS(X) alignas(X)
+#elif ANNOTATORPLUGIN_COMPILER_IS_GNU || ANNOTATORPLUGIN_COMPILER_IS_Clang || \
+    ANNOTATORPLUGIN_COMPILER_IS_AppleClang
+#define ANNOTATORPLUGIN_ALIGNAS(X) __attribute__((__aligned__(X)))
+#elif ANNOTATORPLUGIN_COMPILER_IS_MSVC
+#define ANNOTATORPLUGIN_ALIGNAS(X) __declspec(align(X))
+#else
+#define ANNOTATORPLUGIN_ALIGNAS(X)
+#endif
+
+#if ANNOTATORPLUGIN_COMPILER_CXX_ALIGNOF
+#define ANNOTATORPLUGIN_ALIGNOF(X) alignof(X)
+#elif ANNOTATORPLUGIN_COMPILER_IS_GNU || ANNOTATORPLUGIN_COMPILER_IS_Clang || \
+    ANNOTATORPLUGIN_COMPILER_IS_AppleClang
+#define ANNOTATORPLUGIN_ALIGNOF(X) __alignof__(X)
+#elif ANNOTATORPLUGIN_COMPILER_IS_MSVC
+#define ANNOTATORPLUGIN_ALIGNOF(X) __alignof(X)
+#endif
+
+#if ANNOTATORPLUGIN_COMPILER_CXX_CONSTEXPR
+#define ANNOTATORPLUGIN_CONSTEXPR constexpr
+#else
+#define ANNOTATORPLUGIN_CONSTEXPR
+#endif
+
+#if ANNOTATORPLUGIN_COMPILER_CXX_FINAL
+#define ANNOTATORPLUGIN_FINAL final
+#else
+#define ANNOTATORPLUGIN_FINAL
+#endif
+
+#if ANNOTATORPLUGIN_COMPILER_CXX_NOEXCEPT
+#define ANNOTATORPLUGIN_NOEXCEPT noexcept
+#define ANNOTATORPLUGIN_NOEXCEPT_EXPR(X) noexcept(X)
+#else
+#define ANNOTATORPLUGIN_NOEXCEPT
+#define ANNOTATORPLUGIN_NOEXCEPT_EXPR(X)
+#endif
+
+#if ANNOTATORPLUGIN_COMPILER_CXX_NULLPTR
+#define ANNOTATORPLUGIN_NULLPTR nullptr
+#else
+#define ANNOTATORPLUGIN_NULLPTR static_cast<void*>(0)
+#endif
+
+#if ANNOTATORPLUGIN_COMPILER_CXX_THREAD_LOCAL
+#define ANNOTATORPLUGIN_THREAD_LOCAL thread_local
+#elif ANNOTATORPLUGIN_COMPILER_IS_GNU || ANNOTATORPLUGIN_COMPILER_IS_Clang || \
+    ANNOTATORPLUGIN_COMPILER_IS_AppleClang
+#define ANNOTATORPLUGIN_THREAD_LOCAL __thread
+#elif ANNOTATORPLUGIN_COMPILER_IS_MSVC
+#define ANNOTATORPLUGIN_THREAD_LOCAL __declspec(thread)
+#else
+// ANNOTATORPLUGIN_THREAD_LOCAL not defined for this configuration.
+#endif
+
+#endif
+
+#endif
